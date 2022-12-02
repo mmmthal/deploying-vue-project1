e9690e27bcd81700b439f05af1200b33dc589bd1
mmmthal <mothalhatsukaze@outlook.com>
2022-12-01 19:16:53 -0800

    update

--- vue.config.js
+++ vue.config.js
@@ -3,8 +3,9 @@ const path = require('path');
 function resolveSrc(_path) {
   return path.join(__dirname, _path);
 }
-// vue.config.js
+vue.config.js
 module.exports = {
+  publicPath: process.env.NODE_ENV === "production" ? "/deploying-vue-project1/" : "/",
   lintOnSave: true,
   configureWebpack: {
     // Set up all the aliases we use in our app.
@@ -19,3 +20,8 @@ module.exports = {
     sourceMap: process.env.NODE_ENV !== 'production'
   }
 };
+
+export default defineConfig({
+  base: '/deploying-vue-project1/',
+  plugins: [vue()]
+})
